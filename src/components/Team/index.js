91332938cc87a216532a78e1d3aa279c5aac4230
munakalasiva <munakalasiva016@gmail.com>
2024-06-27 16:06:42 +0530

    new1

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,5 +1,11 @@
 
 import "./index.css";
+import md from "../../images/md.JPG";
+import kriti from "../../images/kriti.jpg";
+import shiva from "../../images/sivq.jpg";
+import siddu from "../../images/siggu.jpg";
+import ravi from "../../images/ravi.JPG";
+import sharmishta from "../../images/sharmishta.jpg";
 
 const Team=()=>{
     return(
@@ -8,7 +14,7 @@ const Team=()=>{
               <p className="team-para">"Meet our accomplished team at Glignesis Technologies, dedicated to innovation and excellence. With expertise spanning diverse domains, we deliver solutions that drive success."endisse vel, sed quam nulla mauris iaculis. Erat eget vitae malesuada, tortor tincidunt porta lorem lectus.</p>
              <div className="cards-cont">
               <div className="image-container">
-                   <img src="https://glignesis.com/images/md.JPG" alt="img" className="image"/>
+                   <img src={md} alt="img" className="image"/>
                    <h1 className="overlay-text">SATHYA SAI CHARAN<br/>FOUNDER & DIRECTOR</h1>
                    
               </div>
@@ -16,28 +22,28 @@ const Team=()=>{
               
 
               <div className="image-container">
-                   <img src="https://glignesis.com/images/kriti.jpg" alt="img" className="image"/>
+                   <img src={kriti} alt="img" className="image"/>
                    <h1 className="overlay-text">KRITI<br/>CO-FOUNDER& DIRECTOR</h1>
                    
               </div>
 
               <div className="image-container">
-                   <img src="https://glignesis.com/images/sivq.jpg" alt="img" className="image"/>
+                   <img src={shiva} alt="img" className="image"/>
                    <h1 className="overlay-text">SHIVA SAI TEJA<br/>FLUTTER DEVELOPER</h1>
               </div>
 
               <div className="image-container">
-                   <img src="https://glignesis.com/images/ravi.JPG" alt="img" className="image"/>
+                   <img src={siddu} alt="img" className="image"/>
                    <h1 className="overlay-text">RAVI TEJA MEKALA<br/>BACK-END</h1>
               </div>
 
               <div className="image-container">
-                   <img src="https://glignesis.com/images/sigguu.jpg" alt="img" className="image"/>
+                   <img src={ravi} alt="img" className="image"/>
                    <h1 className="overlay-text">SIDDU PALITHYA<br/>WEB DEVELOPER</h1>
               </div>
 
               <div className="image-container">
-                   <img src="https://glignesis.com/images/sharmishta.jpg" alt="img" className="image"/>
+                   <img src={sharmishta} alt="img" className="image"/>
                    <h1 className="overlay-text">SHARMISHTA<br/>BACK END</h1>
               </div>
 
