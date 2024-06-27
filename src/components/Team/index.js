1df940324a11e3ec7b88f0abda7f48ac7178d2f6
munakalasiva <munakalasiva016@gmail.com>
2024-06-27 22:30:08 +0530

    router changed

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -13,7 +13,7 @@ const Team=()=>{
           <div className="team-cont">
               <h1>OUR TEAM</h1>
               <p className="team-para">"Meet our accomplished team at Glignesis Technologies, dedicated to innovation and excellence. With expertise spanning diverse domains, we deliver solutions that drive success."endisse vel, sed quam nulla mauris iaculis. Erat eget vitae malesuada, tortor tincidunt porta lorem lectus.</p>
-             <div className="cards-cont">
+             <div className="team-cards">
               <div className="image-container">
                    <img src={md} alt="img" className="image"/>
                    <h1 className="overlay-text">SATHYA SAI CHARAN<br/>FOUNDER & DIRECTOR</h1>
