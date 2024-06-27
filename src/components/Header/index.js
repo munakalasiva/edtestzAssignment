c1a974cb70e2afc2f649a34c70aff57da64655f1
munakalasiva <munakalasiva016@gmail.com>
2024-06-27 22:55:56 +0530

    done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-
+import {Link} from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -23,12 +23,12 @@ const Header=()=>{
           <Navbar.Toggle classsName="custome-toggler" aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/" className="nav-menu-item">Home</Nav.Link>
-              <Nav.Link href="/services" className="nav-menu-item">Services</Nav.Link>
-              <Nav.Link href="/careers" className="nav-menu-item">Careers</Nav.Link>
-              <Nav.Link href="/about" className="nav-menu-item">About</Nav.Link>
-              <Nav.Link href="/team" className="nav-menu-item">Team</Nav.Link>
-              <Nav.Link href="/contact us" className="nav-menu-item">Contact us</Nav.Link>
+              <Nav.Link as={Link} to="/" className="nav-menu-item">Home</Nav.Link>
+              <Nav.Link as={Link} to="/services" className="nav-menu-item">Services</Nav.Link>
+              <Nav.Link as={Link} to="/careers" className="nav-menu-item">Careers</Nav.Link>
+              <Nav.Link as={Link} to="/about" className="nav-menu-item">About</Nav.Link>
+              <Nav.Link as={Link} to="/team" className="nav-menu-item">Team</Nav.Link>
+              <Nav.Link as={Link} to="/contact-us" className="nav-menu-item">Contact Us</Nav.Link>
             </Nav>
             
           </Navbar.Collapse>
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 import './index.css'; // Assuming you have an index.css file for global styles
 
@@ -26,8 +26,8 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
